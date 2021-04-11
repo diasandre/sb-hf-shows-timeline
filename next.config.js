6f85521c797455c6fc30fa9144529d99f39420ec
andré <andredds12@gmail.com>
2021-04-11 00:35:01 -0300

    add next optimize image

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
-const withImages = require('next-images')
-module.exports = withImages()
+const withImages = require("next-images");
+
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      handleImages: ["gif"],
+      gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+      },
+    },
+  ],
+  withImages(),
+]);
