6ac4b991565b6c7b94900e5002e7f74508a797ca
andré <andredds12@gmail.com>
2021-04-11 01:32:25 -0300

    remove next optimize because didnt work

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 const withImages = require("next-images");
 
 const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      handleImages: ["gif"],
-      gifsicle: {
-        interlaced: true,
-        optimizationLevel: 3,
-      },
-    },
-  ],
-  withImages(),
-]);
+module.exports = withPlugins([withImages()]);
